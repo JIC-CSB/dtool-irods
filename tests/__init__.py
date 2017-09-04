159c329f24575e9f3ca45c6b41ba6e819336843a
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-09-04 16:49:11 +0100

    Add test fixture for irods dataset

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,17 @@ import tempfile
 
 import pytest
 
+from dtoolcore import generate_admin_metadata
+from dtool_irods.storagebroker import (
+    _rm_if_exists,
+    IrodsStorageBroker,
+)
+
+
 _HERE = os.path.dirname(__file__)
+TEST_SAMPLE_DATA = os.path.join(_HERE, "data")
+
+TEST_ZONE = "/jic_archive"
 
 
 @pytest.fixture
@@ -39,3 +49,18 @@ def local_tmp_dir_fixture(request):
     def teardown():
         shutil.rmtree(d)
     return d
+
+
+@pytest.fixture
+def tmp_uuid_and_uri(request):
+    admin_metadata = generate_admin_metadata("my_ds")
+    uuid = admin_metadata["uuid"]
+
+    uri = IrodsStorageBroker.generate_uri("my_ds", uuid, TEST_ZONE)
+
+    @request.addfinalizer
+    def teardown():
+        _, irods_path = uri.split(":", 1)
+        _rm_if_exists(irods_path)
+
+    return (uuid, uri)
