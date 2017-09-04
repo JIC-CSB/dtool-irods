a8309ac48ef703c1a77b565e8dd26795ebd244c4
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-09-04 16:20:55 +0100

    Specify dtoolcore version requirement

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     download_url="{}/tarball/{}".format(url, version),
     install_requires=[
         "click",
-        "dtoolcore",
+        "dtoolcore>=2.2.0",
         "dtool_cli",
     ],
     entry_points={
