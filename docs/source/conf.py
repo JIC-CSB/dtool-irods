2fa8ae5fbfb61b1d44d95bd0aca924cea2f06e03
Tjelvar Olsson <olssont@nbi.ac.uk>
2020-03-19 17:17:05 +0000

    Update version number to 0.10.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ repo_name = u"dtool_irods"
 # built documents.
 #
 # The short X.Y version.
-version = u"0.9.2"
+version = u"0.10.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- dtool_irods/__init__.py
+++ dtool_irods/__init__.py
@@ -4,7 +4,7 @@ import sys
 import logging
 from subprocess import Popen, PIPE
 
-__version__ = "0.9.2"
+__version__ = "0.10.0"
 
 logger = logging.getLogger(__name__)
 
